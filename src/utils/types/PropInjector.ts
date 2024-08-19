f07fae64264459fb3d1acdbb236540cbe0c00e52
Alexandr Petyaykin <saneksa2@gmail.com>
2024-08-19 10:35:54 +0300

    feat: добавлена возможность хокам принимать несколько аргументов

--- src/utils/types/PropInjector.ts
+++ src/utils/types/PropInjector.ts
@@ -1,9 +1,12 @@
 import type * as hoistNonReactStatics from "hoist-non-react-statics";
 
-export type TPropInjector<InjectedProps, AdditionalProps = unknown> = <
-  C extends React.ComponentType<React.ComponentProps<C> & InjectedProps>,
->(
+export type TPropInjector<
+  InjectedProps,
+  AdditionalProps = unknown,
+  ExtraArgs extends any[] = [],
+> = <C extends React.ComponentType<React.ComponentProps<C> & InjectedProps>>(
   component: C,
+  ...args: ExtraArgs
 ) => React.ComponentType<
   Omit<
     JSX.LibraryManagedAttributes<C, React.ComponentProps<C>>,
